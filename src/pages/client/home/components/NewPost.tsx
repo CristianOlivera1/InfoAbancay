52bba593d12e95d153d5b068317cfb423708146b
Codideep ... <supportaccount@codideep.com>
2025-11-09 20:43:06 -0500

    feat: create NewPost component to display PostWithoutImage and CardPost

---
+++ src/pages/client/home/components/NewPost.tsx
@@ -0,0 +1,12 @@
+import PostWhithoutImage from './PostWhithoutImage.tsx';
+import CardPost from './CardPost.tsx';
+
+export default function NewPost() {
+    
+    return (
+        <section id="loultimo" className="mb-20">
+            <PostWhithoutImage />
+            <CardPost />
+        </section>
+    );
+}
