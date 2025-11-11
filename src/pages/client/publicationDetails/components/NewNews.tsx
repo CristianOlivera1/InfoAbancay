8e7cb73c46ea79b03eb0af76fef699a0f214664c
Codideep ... <supportaccount@codideep.com>
2025-11-11 18:23:48 -0500

    feat: add new news item with title and date to NewNews component

--- src/pages/client/publicationDetails/components/NewNews.tsx
+++ src/pages/client/publicationDetails/components/NewNews.tsx
@@ -16,6 +16,12 @@ export default function NewNews() {
                         <p className="text-sm font-bold leading-snug text-[#111418]    transition-colors">Public Library Launches Digital Literacy Program</p>
                         <p className="text-xs text-gray-500  mt-1">October 24, 2023</p>
                     </div>
+                </a><a className="flex items-center gap-4 group" href="#">
+                    <div className="w-20 h-20 bg-center bg-no-repeat bg-cover rounded-lg shrink-0" data-alt="Modern library interior" style={{ backgroundImage: "url('https://lh3.googleusercontent.com/aida-public/AB6AXuAaG2rlDTnt0ruZ0OXzM2PiNncHv5-y9s2BZ3_aH8_ILtLjhO_yziY0O9ZZGXr-zWINyIkzqs1GfpX5J8HKU1brrpJeEGl-PGcfcFfPFjjdhyj842PYi2mcBaooABHGGKmi40RWEh26Kiyhilv8gA8RXmv5-7owo9Tuhjo7Z5knRpr7zENlN1n33tCedK37NGrh6_32XBCIoMti4a7ZiLRLe0VKCMqbgwQC1bn6oNKUgoOb_f_d1F5-CN9SnJMFvyUF2Q1gO0Ur1A')" }}></div>
+                    <div>
+                        <p className="text-sm font-bold leading-snug text-[#111418]    transition-colors">Public Library Launches Digital Literacy Program</p>
+                        <p className="text-xs text-gray-500  mt-1">October 24, 2023</p>
+                    </div>
                 </a>
             </div>
         </div>
