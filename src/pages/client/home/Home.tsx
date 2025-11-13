67081b2712998fd93d1b4c620605f32e60aaf542
CristianOlivera1 <oliverachavezcristian@gmail.com>
2025-11-13 13:56:19 -0500

    feat: add smooth scrolling effect for hash links in Home component

--- src/pages/client/home/Home.tsx
+++ src/pages/client/home/Home.tsx
@@ -1,9 +1,18 @@
-
+import { useEffect } from 'react';
 import Slides from './components/Slides.tsx';
 import Categories from './components/Categories.tsx';
 import NewPost from './components/NewPost.tsx';
 
 export default function Home() {
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const element = document.querySelector(hash);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, []);
 
   return (
     <div className="container mx-auto px-4 sm:px-6 xl:px-32 mt-8">
