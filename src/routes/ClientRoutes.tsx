3c072f578e875feb34b40b0a65d8bc5dd08dc1b4
CristianOlivera1 <oliverachavezcristian@gmail.com>
2025-12-09 15:41:49 -0500

    fix: update MyPublications route to correctly reference the MyPublications component

--- src/routes/ClientRoutes.tsx
+++ src/routes/ClientRoutes.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Header from '../shared/client/header/Header';
 import Footer from '../shared/client/footer/Footer';
 import { lazy } from 'react';
+import MyPublications from '../pages/client/myPublications/MyPublications';
 
 const Home = lazy(() => import('../pages/client/home/Home'));
 const PublicationDetails = lazy(() => import('../pages/client/publicationDetails/PublicationDetails'));
@@ -15,16 +16,16 @@ export default function ClientRoutes() {
     <div className="min-h-screen flex flex-col">
       <Header />
       <main className="grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/publicacion/:idPublication" element={<PublicationDetails />} />
-            <Route path="/iniciar-sesion" element={<Login />} />
-            <Route path="/registrarse" element={<Register />} />
-            <Route path="/insertar-publicacion" element={<InsertPublication />} />
-            <Route path="/categoria/:nameCategory" element={<CategoryPosts />} />
-            <Route path="/detalles-publicacion" element={<PublicationDetails />} />
-            <Route path="/mis-publicaciones" element={<PublicationDetails />} />
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/publicacion/:idPublication" element={<PublicationDetails />} />
+          <Route path="/iniciar-sesion" element={<Login />} />
+          <Route path="/registrarse" element={<Register />} />
+          <Route path="/crear-publicacion" element={<InsertPublication />} />
+          <Route path="/categoria/:nameCategory" element={<CategoryPosts />} />
+          <Route path="/detalles-publicacion" element={<PublicationDetails />} />
+          <Route path="/mis-publicaciones" element={<MyPublications />} />
+        </Routes>
       </main>
       <Footer />
     </div>
